4c1643930968c330ae2d362161a025ea2f0e096c
David Lozano <dawgun@hotmail.com>
2022-07-13 19:40:30 +0200

    Fixed eslint advertisements

--- public/Calculadora/Calculadora con prompt.js
+++ public/Calculadora/Calculadora con prompt.js
@@ -3,11 +3,28 @@ const raizResultado = [];
 const numResultado = [];
 let añadirNumero;
 
+function numCalculadora(num1, num2) {
+  numResultado.push(Math.floor((Number(num1) + Number(num2)) * 1000) / 1000);
+  numResultado.push(Math.floor((Number(num1) - Number(num2)) * 1000) / 1000);
+  numResultado.push(Math.floor(Number(num1) * Number(num2) * 1000) / 1000);
+  numResultado.push(Math.floor((Number(num1) / Number(num2)) * 1000) / 1000);
+  console.log(`La suma de ${num1} y ${num2} es ${numResultado[0]}`);
+  console.log(`La resta de ${num1} y ${num2} es ${numResultado[1]}`);
+  console.log(`La multiplicación de ${num1} y ${num2} es ${numResultado[2]}`);
+  console.log(`La división de ${num1} y ${num2} es ${numResultado[3]}`);
+}
+
+function raizCuadrada(num1) {
+  raizResultado.push(Math.floor(Math.sqrt(num1) * 1000) / 1000);
+  console.log(`La raiz cuadrada de ${num1} es ${raizResultado[0]}`);
+}
+
 while (añadirNumero !== null && listaNumeros.length < 2) {
   añadirNumero = prompt("Añade un numero");
 
   if (añadirNumero === null) {
-  } else if (!isNaN(añadirNumero) && añadirNumero.length !== 0) {
+    alert("Que tengas un buen dia");
+  } else if (!Number.isNaN(añadirNumero) && añadirNumero.length !== 0) {
     listaNumeros.push(Number(añadirNumero));
   } else {
     alert(
@@ -18,24 +35,8 @@ while (añadirNumero !== null && listaNumeros.length < 2) {
 
 if (listaNumeros.length === 0) {
   alert("No has introducido ningun número");
-} else if (listaNumeros.length == 1) {
+} else if (listaNumeros.length === 1) {
   raizCuadrada(listaNumeros[0]);
 } else {
   numCalculadora(listaNumeros[0], listaNumeros[1]);
-}
-
-function raizCuadrada(num1) {
-  raizResultado.push(Math.floor(Math.sqrt(num1) * 1000) / 1000);
-  console.log(`La raiz cuadrada de ${num1} es ${raizResultado[0]}`);
-}
-
-function numCalculadora(num1, num2) {
-  numResultado.push(Math.floor((Number(num1) + Number(num2)) * 1000) / 1000);
-  numResultado.push(Math.floor((Number(num1) - Number(num2)) * 1000) / 1000);
-  numResultado.push(Math.floor(Number(num1) * Number(num2) * 1000) / 1000);
-  numResultado.push(Math.floor((Number(num1) / Number(num2)) * 1000) / 1000);
-  console.log(`La suma de ${num1} y ${num2} es ${numResultado[0]}`);
-  console.log(`La resta de ${num1} y ${num2} es ${numResultado[1]}`);
-  console.log(`La multiplicación de ${num1} y ${num2} es ${numResultado[2]}`);
-  console.log(`La división de ${num1} y ${num2} es ${numResultado[3]}`);
 }
