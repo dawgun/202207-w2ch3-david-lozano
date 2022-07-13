de4d0d1ed9868433960c6c1c015d069cd17cf69c
David Lozano <dawgun@hotmail.com>
2022-07-13 19:19:28 +0200

    Configured eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,15 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
+    node: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ["airbnb-base", "prettier"],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {
+    "no-plusplus": "off",
   },
 };
